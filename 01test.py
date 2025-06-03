62bf739866d8c42a23f4c12e9129ae2fc6c1cfe8
Juan E. Peralta <43978385+peraltajuan@users.noreply.github.com>
2025-06-02 21:13:02 -0400

    Update 01test.py

--- 01test.py
+++ 01test.py
@@ -5,7 +5,7 @@ sys = 'SO2.xyz'
 con_mat = ['(1-2)-(2-2)','(1-3)-(2-2)\n']
 pyfodmc.write_pyfodmc_molecules(sys=sys,con_mat=con_mat)
 # Fortran call
-pyfodmc.get_guess('PyFLOSIC','SO2_FODs.xyz')
+pyfodmc.get_guess('PyFLOSIC','SO2_FOD.xyz')
 
 
 
